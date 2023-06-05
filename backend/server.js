a52c51ad4c6c21340a27f9ef7277e3f88df4a110
Marie Cari <mcari@nyc.yearup.org>
2023-06-05 17:24:47 -0400

    added get method to show all data

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,8 @@ app.get("/", (req,res) => {
     res.send("Home page")
  })
  
- //Create a Task
- app.post("/api/tasks", async (req, res) => {
+ //Create a Food and Zipcode Entry
+ app.post("/api/foods", async (req, res) => {
     try {
         const food = await Food.create(req.body)
         res.status(200).json(food)
@@ -28,7 +28,17 @@ app.get("/", (req,res) => {
     }
  })
 
-
+ //Read data
+app.get("/api/foods", async (req,res) => {
+    try {
+        //whenever you want to refer to somehting in your database must use Model to connect (in this example Food)
+        //.find(), you can add parameters inside () on what to find, otherwise leaving blank will return all data
+        const foods = await Food.find()
+        res.status(200).json(foods)
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+    }
+})
 const PORT = process.env.port || 8000
 
 const startServer = async () => {
