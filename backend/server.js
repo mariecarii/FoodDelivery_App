34d0b9e54bdc6822dde2047314685e4341eb3df7
Marie Cari <mcari@nyc.yearup.org>
2023-06-05 16:29:03 -0400

    mongoDB connect with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
+const dotenv = require("dotenv").config()
 const express = require("express")
+const connectDB = require("./config/connectDB")
 
 const app = express()
 
-const PORT = process.env.port || 8000
 
 //Routes
 app.get("/", (req,res) => {
     res.send("Home page")
  })
  
+//mongoDB connection
+connectDB()
+
+const PORT = process.env.port || 8000
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
