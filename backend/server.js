ed8a099c09464126de12b159a5c63d4fefbff505
Marie Cari <mcari@nyc.yearup.org>
2023-06-05 16:58:52 -0400

    added simple middleware, which console.logs data in json

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,26 @@ const connectDB = require("./config/connectDB")
 
 const app = express()
 
+//Middleware
+app.use(express.json())
+// const logger = (req, res, next) => {
+//     console.log("Middleware ran")
+//     next()
+// }
 
 //Routes
 app.get("/", (req,res) => {
     res.send("Home page")
  })
  
+ //Create a Task
+ app.post("/api/tasks", async (req, res) => {
+    console.log(req.body)
+    res.send("Task Created")
+ })
+
 
 const PORT = process.env.port || 8000
-
 
 const startServer = async () => {
     try {
